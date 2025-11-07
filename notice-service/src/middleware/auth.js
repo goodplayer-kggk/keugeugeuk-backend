33f83474bf552334e5db062d7cf649076baca7e0
goodplayer-kggk <keugeugeuk@gmail.com>
2025-11-08 00:02:33 +0900

    verifying firebase admin
    Signed-off-by: goodplayer-kggk <keugeugeuk@gmail.com>

--- notice-service/src/middleware/auth.js
+++ notice-service/src/middleware/auth.js
@@ -1,9 +1,28 @@
 import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import admin from "firebase-admin";
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
+
+const sMServiceClient = new SecretManagerServiceClient();
+export async function getSecret(secretName) {
+  try {
+    const [version] = await sMServiceClient.accessSecretVersion({
+      name: `projects/${process.env.GCP_PROJECT}/secrets/${secretName}/versions/latest`,
+    });
+
+    const payload = version.payload.data.toString("utf8");
+    return payload;
+  } catch (err) {
+    console.error(`❌ Failed to access secret ${secretName}:`, err.message);
+    throw err;
+  }
+}
+
+const serviceAccountStr = await getSecret("GCP_SA_KEY_JSON");
+const serviceAccount = JSON.parse(serviceAccountStr);
 
 admin.initializeApp({
-  credential: applicationDefault(),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = getFirestore();
